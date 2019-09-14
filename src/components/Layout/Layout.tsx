45890d3921b44f91612a1f106d23c6ecdee92b0d
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-09-14 23:19:38 +1200

    add logo

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,9 @@
 import { Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, ListSubheader } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import DashboardIcon from '@material-ui/icons/Dashboard';
 import FaceIcon from '@material-ui/icons/Face';
 import LanguageIcon from '@material-ui/icons/Language';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
+import clsx from 'clsx';
 import Link from 'next/link';
 import React, { FC } from 'react';
 import { GoRepo } from 'react-icons/go';
@@ -30,6 +31,14 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
       backgroundColor: theme.palette.background.default,
     },
+    brand: {
+      display: 'grid',
+      alignItems: 'center',
+      padding: theme.spacing(2),
+    },
+    brandImage: {
+      width: '100%',
+    },
   }),
 );
 
@@ -46,16 +55,18 @@ const Layout: FC = ({ children }) => {
         }}
         anchor="left"
       >
-        <div className={classes.toolbar} />
+        <div className={clsx([classes.toolbar, classes.brand])}>
+          <img src="/static/logo.png" className={classes.brandImage} />
+        </div>
         <Divider />
         <List>
           <Link href="/">
-          <ListItem button>
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary="Dashboard" />
-          </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItem>
           </Link>
         </List>
         <Divider />
--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -5,7 +5,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#E6371C',
     },
     secondary: {
       main: '#19857b',
