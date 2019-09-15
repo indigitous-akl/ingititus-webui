f3268368caf2fdc57762dea663180b568835aa1f
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-09-15 12:30:03 +1200

    update repository

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -5,5 +5,8 @@ const API_URL = 'ws://10.1.1.70:8182/gremlin';
 const traversal = gremlin.process.AnonymousTraversalSource.traversal;
 const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
 
+export const valueMap = gremlin.process.statics.valueMap;
+export const out = gremlin.process.statics.out;
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default () => traversal().withRemote(new DriverRemoteConnection(API_URL) as any);
